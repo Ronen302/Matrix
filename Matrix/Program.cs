5489727c27ab9fc9d4ef07939e4112e289f73915
Ronen302 <Ronen302@RONEN302>
2021-11-09 19:59:24 +0300

    Добавьте файлы проекта.

---
+++ Matrix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matrix[j, i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
